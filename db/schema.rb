b4cf826d3211aca8f0b971bddc4c5888f31c619c
Max Cantor <max@maxcantor.net>
2014-09-30 15:28:30 -0700

    Add github_token column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929215659) do
+ActiveRecord::Schema.define(version: 20140930222804) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "github_token"
   end
 
 end
